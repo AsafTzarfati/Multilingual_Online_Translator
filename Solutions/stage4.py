a9203c9ec021b868e2909ac3c265a0210127f7b5
Asaf Tzarfati <asaff19899@gmail.com>
2022-02-22 22:21:37 +0200

    Create stage4.py
    Description
    Great job! You now have a basic translation app that works well. Wouldn’t it be great though to expand it and include all available languages? This will finally make our translator a multilingual one!
    
    The maximum number of languages our translator can support is 13. They are:
    
    Arabic
    German
    English
    Spanish
    French
    Hebrew
    Japanese
    Dutch
    Polish
    Portuguese
    Romanian
    Russian
    Turkish
    They should be enumerated in the program. A great idea is to present them with relevant numbers so that the user can choose the first as the original language and the second as a translation.
    
    Objectives
    At this stage, your program should:
    
    Output the welcoming message (let's update it a bit): Hello, you're welcome to the translator. Translator supports:
    Output an enumerated list of languages. The enumeration should start from 1. The order of languages should be exactly as in the list above.
    Take input (a number from the list) specifying the source language (the language from which the translation should be performed).
    Take input (a number from the list) specifying the target language (the language to which the translation should be performed).
    Take input specifying the word that should be translated.
    Output the results as in the previous stage. At this stage, you don't need to print 200 OK anymore.
    Tip 1: just place the listed languages into the URL depending on the user’s choice!
    
    Tip 2: Try to convert the input to lower case: it may cause an error if the user's input is in upper case or mixed.

---
+++ Solutions/stage4.py
@@ -0,0 +1,92 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+SUCCESS = 200
+NUM_OF_WORDS = 5
+NUM_OF_EXAMPLES = 5
+FIRST_INDEX = 1
+WEB_BASE = 'https://context.reverso.net/translation/'
+ALL_LANGUAGES = ["Start", "Arabic", "German", "English", "Spanish", "French", "Hebrew", "Japanese",
+                 "Dutch", "Polish", "Portuguese", "Romanian", "Russian", "Turkish"]
+
+
+class Translator:
+
+    src_language = None
+    trg_language = None
+    url = None
+    path = None
+    word = None
+    word_list = None
+    examples_list = None
+
+    def __init__(self):
+
+        print("Hello, you're welcome to the translator. Translator supports:")
+        for i, each_language in enumerate(ALL_LANGUAGES):
+            if i != 0:
+                print(f"{i}. {each_language}")
+
+    def display(self, num_of_words, num_of_examples):
+
+        print(f'You chose "{self.trg_language}" as a language to translate "{self.word}".')
+        print(f"\n{self.trg_language} Translations:\n")
+
+        # Print translated words
+        for word in self.word_list[:num_of_words]:
+            print(word)
+
+        print(f"\n{self.trg_language} Examples:\n")
+        # Multiply by two come from source language and target language examples
+        for index in range(0, num_of_examples * 2, 2):
+            print(f'{self.examples_list[index]}\n{self.examples_list[index + 1]}\n\n')
+
+    def menu(self):
+
+        self.src_language = ALL_LANGUAGES[int(input('Type the number of your language:'))]
+        self.trg_language = ALL_LANGUAGES[int(input("Type the number of language you want to translate to:"))]
+        self.word = input('Type the word you want to translate:')
+
+    def build_url(self):
+
+        self.url = WEB_BASE + f'{self.src_language}-{self.trg_language}/' + self.word
+        self.url = self.url.lower()
+
+    def scrap_answer(self, r):
+        soup = BeautifulSoup(r.content, "html.parser")
+        # Find all translated words by css id translations and tag a
+        translation_words = soup.select("#translations-content a")
+        self.word_list = [each_word.text.strip() for each_word in translation_words]
+        # Find all examples by css id examples-content and class text
+        translation_sentences = soup.select("#examples-content .text")
+        self.examples_list = [sentence.text.strip() for sentence in translation_sentences]
+
+    def translate_word(self):
+        self.build_url()
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        r = requests.get(self.url, headers=headers)
+        if r.status_code == SUCCESS:
+            self.scrap_answer(r)
+
+    def translate(self):
+        if self.trg_language == "All":
+            for language in ALL_LANGUAGES[FIRST_INDEX:]:
+                self.trg_language = language
+                if self.trg_language != self.src_language:
+                    self.translate_word()
+                    self.display(NUM_OF_WORDS, NUM_OF_EXAMPLES)
+        else:
+            self.translate_word()
+            self.display(NUM_OF_WORDS, NUM_OF_EXAMPLES)
+
+    def start(self):
+        self.menu()
+        self.translate()
+
+
+if __name__ == '__main__':
+
+    translator = Translator()
+    translator.start()
+
